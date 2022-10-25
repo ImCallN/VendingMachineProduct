10cea262ea4abccb3ee074cc6ec7b528578219ac
ImCallN <collindunkle1@gmail.com>
2022-10-24 20:27:22 -0700

    -Made many change yes. -Mostly focused on the slot and vending machine objects here -Began testing within Main -Investigating differences within Slot with using Arrays vs Arraylists -Comments on todo items and for readability

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,41 @@
 public class Main {
     public static void main(String [] args)
     {
-        System.out.println("Welcome to xyz Vending Machine Software Product! Are you a customer, restocker, or management?");
+        //System.out.println("Welcome to xyz Vending Machine Software Product! Are you a customer, restocker, or management?");
             //Take input
         //If customer, show customer operations
         //If restocker, show restocker operations
         //Management, show management operations
         //To Exit, type Exit
+
+
+
+        //Testing Stuff
+        VendingMachine a = new VendingMachine(5,4,5);
+        Item b = new Item("100", 1.99);
+        Item d = new Item("40A", 2.99);
+
+        /* 
+        Slot c = new Slot();
+        c.addItem(b, 3);
+        c.addItem(d,3,7); 
+        a.inventory[1][1] = c;
+        System.out.println(a.inventory[1][1].items[2].id);
+        c.moveItemsForward(1);
+        System.out.println(a.inventory[1][1].items[2].id);
+        */
+
+        Slot e = new Slot();
+        e.addItemAList(b);
+        e.addItemAList(d, 3);
+        a.inventory[1][2] = e;
+        System.out.println("Item in second slot:" + " " + a.inventory[1][2].itemsAList.get(1).id);
+        System.out.println("Item in first Slot:" + " " + a.inventory[1][2].itemsAList.get(0).id);
+
+        e.removeItemAList(0);
+        System.out.println("Item in first slot after change:" + " " + a.inventory[1][2].itemsAList.get(0).id);
+
+        
+
     }
 }
--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,23 +1,51 @@
 import java.util.ArrayList;
 
+
 public class VendingMachine
 {
     String location;
-    int column;
-    int row;
+    int columnSize;
+    int rowSize;
     int depth;
     int maxInventory;
     int inventoryAvailable;
     ArrayList<Item> removeList = new ArrayList<Item>();
     ArrayList<Item> addItem = new ArrayList<Item>();
+    Slot inventory[][];
 
     public VendingMachine(int row, int column, int depth)
     {
         //2D array with height = row and width = column of slot objects(constructor with depth)
+         inventory = new Slot[row][column];
+         standardPopulateInventory();
     }
 
     //public custSelect(List)
     //public calculateTotal(double)
     //public heartbeat
-    //public
+    public void standardPopulateInventory()
+    {
+        for(int i = 0; i < rowSize; i++)
+        {
+            for(int j = 0; j < columnSize; j++)
+            {
+                inventory[i][j] = new Slot(depth);
+            }
+        }
+    }
+
+
+    public void populateRemoveList()
+    {
+        //Get current date
+        for(int i = 0; i < rowSize; i++)
+        {
+            for(int j = 0; j < columnSize; j++)
+            {
+                //Call slot function to check expiration dates on items. 
+                //The question is do we return the slot for expired items or do we return all of the individual items?
+                
+            }
+        }
+    }
 }
